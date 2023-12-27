ed5c3ff8c65782f24c665f93ca0924ef8c2b472d
Andrew <bakhcha_ml@mail.ru>
2023-12-27 16:11:47 +0300

    exercise_2

--- main.py
+++ main.py
@@ -1,4 +1,14 @@
-<!DOCTYPE html>
+from http.server import BaseHTTPRequestHandler, HTTPServer
+
+
+hostName = "localhost"
+serverPort = 8080
+
+class MyServer(BaseHTTPRequestHandler):
+
+    def __get_html(self):
+        return """
+        <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="utf-8">
@@ -352,3 +362,22 @@
 </script>
 </body>
 </html>
+"""
+
+    def do_GET(self):
+        self.send_response(200)
+        self.send_header("Content-type", "text/html")
+        self.end_headers()
+        self.wfile.write(bytes(self.__get_html(), "utf-8"))
+
+if __name__ == "__main__":
+    webServer = HTTPServer((hostName, serverPort), MyServer)
+    print("Server started http://%s:%s" % (hostName, serverPort))
+
+    try:
+        webServer.serve_forever()
+    except KeyboardInterrupt:
+        pass
+
+    webServer.server_close()
+    print("Server stopped.")
